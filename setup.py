09cd11faa36984b8c6ef8b9bac8f01b3998d73d5
Dave Panofsky <dpanofsky@singleplatform.com>
2018-07-19 11:56:47 -0400

    move from setup.py to setup.cfg; add pbr; singleplatform additions

--- setup.py
+++ setup.py
@@ -1,23 +1,8 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+
+from setuptools import setup
 
 setup(
-    name='django-celery-admin-ext',
-    version='1.0-mc2',
-    description='A django application that lets you kick off periodic celery tasks from the admin.',
-    long_description=open('README.md').read(),
-    author='Matt Caldwell',
-    author_email='mcaldwel@ngs.org',
-    url='http://github.com/mattcaldwell/django-celery-admin-ext',
-    packages=find_packages(exclude=[]),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ],
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True,
 )
